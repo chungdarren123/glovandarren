ce97d2ef2f832007623941bd6aa8927d77fe53cb
chungdarren <chungdarren123@gmail.com>
2025-06-18 10:48:19 +0800

    refactor directory naming

--- algorithmic_engine/app/scraper/items.py
+++
@@ -1,6 +0,0 @@
-import scrapy
-
-class ScrapedTextItem(scrapy.Item):
-    url = scrapy.Field()
-    content = scrapy.Field()
-    timestamp = scrapy.Field() # Can be set in pipeline or spider
--- algorithmic_engine/app/services/scraping_service_interface.py
+++
@@ -1,7 +0,0 @@
-from abc import ABC, abstractmethod
-
-class ScrapingJobServiceInterface(ABC):
-    @abstractmethod
-    def trigger_scraping_job(self) -> str:
-        '''Triggers a scraping job and returns a message or task ID.'''
-        pass
